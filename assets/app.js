c59ac072b0b21b0a30a5899e8b26f0ae639736b5
MuhammadAhmadAslam <prince1ahmed111@gmail.com>
2024-07-29 13:22:55 +0500

    Commit

---
+++ assets/app.js
@@ -0,0 +1,41 @@
+const budgetInput = document.getElementById('budget-input');
+const set = document.getElementById('set');
+const totalBudgetHere = document.getElementById('totalBudgetHere');
+const balance = document.getElementById('balance');
+const titleInput = document.getElementById('title-input');
+const costInput = document.getElementById('cost-input');
+const check = document.getElementById('check');
+const expenses = document.getElementById('expenses');
+const expenseList = document.getElementById('expense-list')
+set.addEventListener('click' , () => {
+    totalBudgetHere.innerText = budgetInput.value;
+    balance.innerText = budgetInput.value;
+    budgetInput.disabled = true
+})
+
+check.addEventListener('click' , () => {
+        if (totalBudgetHere.innerText != 0) {
+            if (titleInput.value != '' && costInput.value != '') {
+                expenses.innerText = parseFloat(costInput.value) + parseFloat(expenses.innerText);
+                balance.innerText = balance.innerText - costInput.value;
+                expenseList.innerHTML += ` <div class="q2 row">
+                <h3 class="col-sm-4 col-lg-4"><i class="fa-solid fa-trash bin" id='bin'></i> ${titleInput.value}</h3>
+                <h3 class="col-sm-4 col-lg-4">${costInput.value}</h3>
+            </div>`
+                costInput.value = ''
+                titleInput.value = ''
+
+
+
+                const bin = document.querySelectorAll('.bin');
+                bin.forEach((bin,index) => {
+                    bin.addEventListener('click' , () => {
+                        let parentElements = bin.parentElement.parentElement;
+                        console.log(parentElements);
+                        console.log(index);
+                        expenseList.removeChild(parentElements)
+                    })
+                })
+            }
+        }
+})
