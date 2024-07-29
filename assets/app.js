51a3530dface4795f2cd165e7c15f5b5cd74c1b8
MuhammadAhmadAslam <prince1ahmed111@gmail.com>
2024-07-29 16:23:10 +0500

    Commit

--- assets/app.js
+++ assets/app.js
@@ -16,26 +16,46 @@ set.addEventListener('click' , () => {
 check.addEventListener('click' , () => {
         if (totalBudgetHere.innerText != 0) {
             if (titleInput.value != '' && costInput.value != '') {
+            if ((costInput.value - balance.innerText) < 0) {
                 expenses.innerText = parseFloat(costInput.value) + parseFloat(expenses.innerText);
                 balance.innerText = balance.innerText - costInput.value;
                 expenseList.innerHTML += ` <div class="q2 row">
                 <h3 class="col-sm-4 col-lg-4"><i class="fa-solid fa-trash bin" id='bin'></i> ${titleInput.value}</h3>
-                <h3 class="col-sm-4 col-lg-4">${costInput.value}</h3>
+                <h3 class="col-sm-4 col-lg-4 costInputValue">${costInput.value}</h3>
             </div>`
                 costInput.value = ''
                 titleInput.value = ''
 
 
-
+                const costInputValue = document.querySelectorAll('.costInputValue')
                 const bin = document.querySelectorAll('.bin');
                 bin.forEach((bin,index) => {
                     bin.addEventListener('click' , () => {
                         let parentElements = bin.parentElement.parentElement;
-                        console.log(parentElements);
-                        console.log(index);
-                        expenseList.removeChild(parentElements)
+                        balance.innerText = parseFloat(balance.innerText) + parseFloat(costInputValue[index].innerText);
+                        expenses.innerText = parseFloat(expenses.innerText) - parseFloat(costInputValue[index].innerText);
+                        expenseList.removeChild(parentElements);
+                        Swal.fire({
+                            icon: "success",
+                            title: "The item deleted sucessfully",
+                            showConfirmButton: false,
+                            timer: 1500
+                          });
                     })
                 })
+            }else{
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "You are going out of budget",
+                  });
             }
+            }
+        }else{
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "You need to add your budget first",
+              });
         }
 })
